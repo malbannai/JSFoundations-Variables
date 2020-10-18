8556c7fb97d63bda400b5b309ceee99f2298c134
malbannai <moudhi.albannai@gamil.com>
2020-10-18 19:13:58 +0300

    Story time, done

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "Tom"
+let age = 5
+let hoppy = "Sleeping"
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hoppy} practice!`)
